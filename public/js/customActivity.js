c40e33864264f758e6049e8f9432b0dc7dfcf8a1
jainkalpit1997 <39576154+jainkalpit1997@users.noreply.github.com>
2020-04-02 13:37:21 +0530

    Update customActivity.js

--- public/js/customActivity.js
+++ public/js/customActivity.js
@@ -100,7 +100,7 @@ define([
             "authToken": authToken,
             "messagingService": messagingService,
             "body": body,
-            "to": "{{Contact.Attribute.TestDataEx.Phone}}" //<----This should map to your data extension name and phone number column
+            "to": "{{Contact.Synch Data.TestDataEx.Phone}}" //<----This should map to your data extension name and phone number column
         }];
 
         payload['metaData'].isConfigured = true;
