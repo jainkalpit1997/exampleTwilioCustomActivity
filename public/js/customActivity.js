19bd4e923c061a13fbdd46bd6c135159a3baa9cd
jainkalpit1997 <39576154+jainkalpit1997@users.noreply.github.com>
2020-04-04 11:53:43 +0530

    Update customActivity.js

--- public/js/customActivity.js
+++ public/js/customActivity.js
@@ -102,7 +102,7 @@ define([
             "authToken": authToken,
             "messagingService": messagingService,
             "body": body,
-            from": "+18304235288",
+            "from": "+18304235288",
           //  "from": "{{Contact.Attribute.TestDataEx.Phone}}",
          //   "to": "{{Contact.Attribute.TestDataEx.Phone}}" //<----This should map to your data extension name and phone number column
             "to": "+918890338219"
