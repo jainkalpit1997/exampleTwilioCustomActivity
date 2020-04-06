654fe124d79fa9c9b2ad71f41542f4ca09fbfd8d
maenk <mayank.sareen41@gmail.com>
2020-04-06 18:04:19 +0530

    Update app.js

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ app.post('/journeybuilder/save/', activity.save );
 app.post('/journeybuilder/validate/', activity.validate );
 app.post('/journeybuilder/publish/', activity.publish );
 app.post('/journeybuilder/execute/', activity.execute );
-alert('Hi')
+alert('Hi');
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
