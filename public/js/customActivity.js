3dbbc062d66945878708c76ed666514f430a5db6
jainkalpit1997 <39576154+jainkalpit1997@users.noreply.github.com>
2020-04-30 16:39:17 +0530

    Update customActivity.js

--- public/js/customActivity.js
+++ public/js/customActivity.js
@@ -117,8 +117,8 @@ define([
             "authToken": authToken,
             "messagingService": messagingService,
             "body": body,
-           {"to": "{{Contact.Attribute.TestDataExCopy.PhoneNumber}}"},
-        {"From": "{{Contact.Attribute.TestDataExCopy.FromPhoneNumber}}"}
+           "to": "{{Contact.Attribute.TestDataExCopy.PhoneNumber}}",
+        "From": "{{Contact.Attribute.TestDataExCopy.FromPhoneNumber}}"
         }];
 
         payload['metaData'].isConfigured = true;
