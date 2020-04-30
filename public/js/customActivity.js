8d5f5656ca2f88c502aadca798de742b16b13851
jainkalpit1997 <39576154+jainkalpit1997@users.noreply.github.com>
2020-04-30 15:28:12 +0530

    Update customActivity.js

--- public/js/customActivity.js
+++ public/js/customActivity.js
@@ -117,8 +117,8 @@ define([
             "authToken": authToken,
             "messagingService": messagingService,
             "body": body,
-            "to": "{{Contact.Attribute.DEAudience-c078c657-1aed-fd09-fd4b-ca0923860b88.PhoneNumber}}",
-            "From": "{{Contact.Attribute.DEAudience-c078c657-1aed-fd09-fd4b-ca0923860b88.FromPhoneNumber}}"
+            "to": "{{Contact.Attribute.6bab3c3d-028a-ea11-a2e9-1402ec8ca03d.PhoneNumber}}",
+            "From": "{{Contact.Attribute.6bab3c3d-028a-ea11-a2e9-1402ec8ca03d.FromPhoneNumber}}"
         }];
 
         payload['metaData'].isConfigured = true;
